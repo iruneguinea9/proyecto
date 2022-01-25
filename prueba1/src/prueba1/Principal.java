f528771c0f91b0361199abab4a09ea31d1f4be7e
irune <iruneguinea29@gmail.com>
2022-01-25 12:48:25 +0100

    Cuarta validación

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -5,7 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
 		System.out.println("Hola  mundo 2 java-git");
-		System.out.println("Adios");
+
 	}
 
 }
